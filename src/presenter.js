22baf83be248d4c172613cc23b060ecf308ecc49
diegooferruuu <diego.ferrufino@ucb.edu.bo>
2024-08-13 10:01:25 -0400

    Se anadio funcion de multiplicar

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,15 @@
 import sumar from "./sumador";
+import mult from "./mult";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+
+const firstMult = document.querySelector("#primer-numero-mult");
+const secondMult = document.querySelector("#segundo-numero-mult");
+const formMult = document.querySelector("#mult-form");
+const divMult = document.querySelector("#resultado-mult-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +18,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+formMult.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(firstMult.value);
+  const secondNumber = Number.parseInt(secondMult.value);
+
+  divMult.innerHTML = "<p>" + mult(firstNumber, secondNumber) + "</p>";
 });
